e0263aed934ae7b4a7edd418039217ad5667ac97
Omkar_Bahirat <omkarbahirat1992@gmail.com>
2016-10-28 00:12:56 -0700

    Commit_43
    New additions:
    
    Modified:
        Hacker_Rank/sum_vs_xor.c
    
    Fix done:
        Solution is very simple just return the number of zeros after first set MSB.
    
    Note:
        All the TC's works except "1099511627776".

--- Hacker_Rank/sum_vs_xor.c
+++ Hacker_Rank/sum_vs_xor.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-unsigned long long int get_max_val(unsigned long long val) 
+unsigned long long int get_all_one_after_set_MSB_mask(unsigned long long val) 
 {
+    val |= val >> 32;
     val |= val >> 16;
     val |= val >> 8;
     val |= val >> 4;
@@ -11,17 +12,40 @@ unsigned long long int get_max_val(unsigned long long val)
     return val;
 }
 
+unsigned int total_set_bits(unsigned long long n)
+{
+    unsigned int count = 0;
+    unsigned int lookuptable[256] = {
+        #define b2(n) n, n+1, n+1, n+2
+        #define b4(n) b2(n), b2(n+1), b2(n+1), b2(n+2)
+        #define b6(n) b4(n), b4(n+1), b4(n+1), b4(n+2)
+        b6(0), b6(1), b6(1), b6(2)
+    };
+    count += lookuptable[n & 0xff];
+    count += lookuptable[n >> 8 & 0xff];
+    count += lookuptable[n >> 16 & 0xff];
+    count += lookuptable[n >> 24 & 0xff];
+    count += lookuptable[n >> 32 & 0xff];
+    count += lookuptable[n >> 40 & 0xff];
+    count += lookuptable[n >> 48 & 0xff];
+    count += lookuptable[n >> 56];
+    return count;
+}
+
+/*
+Flip all the bits including and after first set MSB.
+Now count number of set bits.
+*/
+unsigned int total_zeros_after_set_MSB(unsigned long long n)
+{
+    n = n ^ get_all_one_after_set_MSB_mask(n);  //Flipping all the bits including and after first set MSB
+    return(total_set_bits(n));  
+}
+
 int main(void)
 {
     unsigned long long n, i, count = 0, limit;
     scanf("%llu", &n);
-    
-    limit = get_max_val(n) - n;
-    for (i = 0; i <= limit;i++) {
-        if ((i + n) == (i ^ n)) {
-            count++;
-        }
-    }
-    printf("%llu\n", count);
+    printf("%u\n", 1 << total_zeros_after_set_MSB(n));
     return 0;
 }
